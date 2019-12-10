0e889fa1865569f3d747374e3b5068257d44c65f
Hammad Tahir <hammad.tahir@boldcommerce.com>
2019-12-10 12:31:17 -0600

    remove send email file and add send email function in job file

--- app/job/job.go
+++ app/job/job.go
@@ -3,6 +3,7 @@ package main
 import (
 	machinery "github.com/RichardKnop/machinery/v1"
 	"github.com/RichardKnop/machinery/v1/config"
+	"github.com/hammadtahirch/golang_basic_app/app/services"
 )
 
 func main() {
@@ -24,4 +25,33 @@ func main() {
 		//todo:handle errors
 	}
 
+}
+
+//SendEmailJob ...
+func SendEmailJob(email string) (string, error) {
+	sender := services.NewSender("hammad.tahir.ch@gmail.com", "Honey@03227058541")
+
+	//The receiver needs to be in slice as the receive supports multiple receiver
+	Receiver := []string{"hammad.tahir.ch+testing@gmail.com", "hammad.tahir.ch+testing2@gmail.com", "hammad.tahir.ch+testing3@gmail.com"}
+
+	Subject := "Testing HTLML Email from golang"
+	message := `
+	<!DOCTYPE HTML PULBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+	<html>
+	<head>
+	<meta http-equiv="content-type" content="text/html"; charset=ISO-8859-1">
+	</head>
+	<body>This is the body<br>
+	<div class="moz-signature"><i><br>
+	<br>
+	Regards<br>
+	Alex<br>
+	<i></div>
+	</body>
+	</html>
+	`
+	bodyMessage := sender.WriteHTMLEmail(Receiver, Subject, message)
+
+	sender.SendMail(Receiver, Subject, bodyMessage)
+	return "Hello " + email + "!", nil
 }
