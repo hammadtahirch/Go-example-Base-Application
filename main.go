7eb2d9690cc18003533b763b5ed0cfcfbac7a5c2
Hammad Tahir <hammad.tahir@boldcommerce.com>
2020-01-02 13:15:15 -0600

    add testing code

--- main.go
+++ main.go
@@ -5,8 +5,7 @@ import (
 	"os"
 
 	"github.com/hammadtahirch/nifty_logix/routes"
-	"github.com/gorilla/mux"
-	"github.com/joho/godotenv"
+	"github.com/russross/blackfriday"
 )
 
 // AppRoute ...
@@ -14,12 +13,28 @@ type AppRoute struct {
 	r routes.AppRoute
 }
 
-// main ...
 func main() {
-	godotenv.Load()
-	m := AppRoute{}
-	a := mux.NewRouter()
-	m.r.Routes(a)
-	a.PathPrefix("/public/").Handler(http.StripPrefix("/public/", http.FileServer(http.Dir("./public/"))))
-	http.ListenAndServe(":"+os.Getenv("APP_PORT"), a)
+	port := os.Getenv("APP_PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	http.HandleFunc("/markdown", GenerateMarkdown)
+	http.Handle("/", http.FileServer(http.Dir("public")))
+	http.ListenAndServe(":"+port, nil)
 }
+
+func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
+	markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
+	rw.Write(markdown)
+}
+
+// main ...
+// func main() {
+// 	godotenv.Load()
+// 	m := AppRoute{}
+// 	a := mux.NewRouter()
+// 	m.r.Routes(a)
+// 	a.PathPrefix("/public/").Handler(http.StripPrefix("/public/", http.FileServer(http.Dir("./public/"))))
+// 	http.ListenAndServe(":"+os.Getenv("APP_PORT"), a)
+// }
